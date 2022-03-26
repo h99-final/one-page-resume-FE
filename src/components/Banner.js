f572c78552ccf46dcd4d161ea8acff312914e1ad
han-woo-jin <97419805+han-woo-jin@users.noreply.github.com>
2022-03-26 17:05:23 +0900

    [Banner] 배경 수정

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -33,10 +33,10 @@ const Banner = () => {
         navigation={true}
         modules={[Parallax, Pagination, Navigation, Autoplay]}
         className="mySwiper"
-        autoplay={{
-          delay: 5000,
-          disableOnInteraction: false,
-        }}
+      // autoplay={{
+      //   delay: 5000,
+      //   disableOnInteraction: false,
+      // }}
       >
         <div
           slot="container-start"
@@ -47,131 +47,152 @@ const Banner = () => {
           data-swiper-parallax="-23%"
         ></div>
         <SwiperSlide>
-          <TextBox>
-            <h1>
-              개발자 포트폴리오 만들기,
-              <br />
-              어디까지 해보셨나요?
-            </h1>
-            <h2>
-              개발자들은 포트폴리오 제작을 위해 평균 n시간을 투자하고 있어요.
-            </h2>
-          </TextBox>
-          <BtnBox>
-            <button
-              onClick={() => {
-                history.replace(
-                  `/write/portfolio/introduce/${userInfo.porfId}`
-                );
-              }}
-            >
-              지금 시작하기
-            </button>
-          </BtnBox>
+          <Box style={{
+            backgroundImage: `url("https://ricefriendimage.s3.ap-northeast-2.amazonaws.com/BG1.png")`,
+          }}>
+            <GifBox>
+              <img
+                style={{}}
+                alt=''
+                src='https://ricefriendimage.s3.ap-northeast-2.amazonaws.com/%E1%84%91%E1%85%A5%E1%84%87%E1%85%B3%E1%86%AF%E1%84%85%E1%85%B5%E1%84%89%E1%85%B5%E1%86%BC.gif' />
+            </GifBox>
+            <TextBox>
+              <h1>
+                개발자 포트폴리오 만들기,
+                <br />
+                어디까지 해보셨나요?
+              </h1>
+              <h2>
+                개발자들은 포트폴리오 제작을 위해 평균 n시간을 투자하고 있어요.
+              </h2>
+            </TextBox>
+            <BtnBox>
+              <button
+                onClick={() => {
+                  history.replace(
+                    `/write/portfolio/introduce/${userInfo.porfId}`
+                  );
+                }}
+              >
+                지금 시작하기
+              </button>
+            </BtnBox>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
-          <TextBox>
-            <h1>
-              개발자의,
-              <br />
-              개발자에 의한,
-              <br />
-              개발자를 위한 서비스
-            </h1>
-            <h2>
-              Portfolio는 내가 보여주고 싶은 프로젝트, 코드, 트러블 슈팅을
-              한눈에 효과적으로 보여주는 유일한 이력서입니다.
-            </h2>
-          </TextBox>
-          <BtnBox>
-            <button
-              onClick={() => {
-                history.replace(
-                  `/write/portfolio/introduce/${userInfo.porfId}`
-                );
-              }}
-            >
-              지금 시작하기
-            </button>
-          </BtnBox>
+          <Box style={{
+            backgroundImage: `url("https://ricefriendimage.s3.ap-northeast-2.amazonaws.com/BG2.png")`,
+          }}>
+            <TextBox>
+              <h1>
+                개발자의,
+                <br />
+                개발자에 의한,
+                <br />
+                개발자를 위한 서비스
+              </h1>
+              <h2>
+                Portfolio는 내가 보여주고 싶은 프로젝트와 트러블 슈팅을<br />
+                한눈에 효과적으로 보여주는 유일한 이력서입니다.
+              </h2>
+            </TextBox>
+            <BtnBox>
+              <button
+                onClick={() => {
+                  history.replace(
+                    `/write/portfolio/introduce/${userInfo.porfId}`
+                  );
+                }}
+              >
+                지금 시작하기
+              </button>
+            </BtnBox>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
-          <TextBox>
-            <h1>
-              10분으로 만드는
-              <br />
-              100시간짜리 나만의 포트폴리오
-            </h1>
-            <h2>
-              내가 등록한 프로젝트를 모아 개성있는 포트폴리오로 만들어 보세요.
-            </h2>
-          </TextBox>
-          <BtnBox>
-            <button
-              onClick={() => {
-                history.replace(
-                  `/write/portfolio/introduce/${userInfo.porfId}`
-                );
-              }}
-            >
-              지금 시작하기
-            </button>
-          </BtnBox>
+          <Box style={{
+            backgroundImage: `url("https://ricefriendimage.s3.ap-northeast-2.amazonaws.com/BG3.png")`,
+          }}>
+            <TextBox>
+              <h1>
+                10분으로 만드는
+                <br />
+                100시간짜리 나만의 포트폴리오
+              </h1>
+              <h2>
+                내가 등록한 프로젝트들을 모아 나의 개성을 드러내는 포트폴리오로 만들어 보세요.
+              </h2>
+            </TextBox>
+            <BtnBox>
+              <button
+                onClick={() => {
+                  history.replace(
+                    `/write/portfolio/introduce/${userInfo.porfId}`
+                  );
+                }}
+              >
+                지금 시작하기
+              </button>
+            </BtnBox>
+          </Box>
         </SwiperSlide>
         <SwiperSlide>
-          <TextBox>
-            <h1>
-              P()UG와 함께라면,
-              <br />
-              개발자 취업, 너도 할 수 있어.
-            </h1>
-            <h2>
-              여기저기 흩어져 있는 포트폴리오를 하나로 만들어 제출해 보세요.
-            </h2>
-          </TextBox>
-          <BtnBox>
-            <button
-              onClick={() => {
-                history.replace(
-                  `/write/portfolio/introduce/${userInfo.porfId}`
-                );
-              }}
-            >
-              지금 시작하기
-            </button>
-          </BtnBox>
+          <Box style={{
+            backgroundImage: `url("https://ricefriendimage.s3.ap-northeast-2.amazonaws.com/BG4.png")`,
+          }}>
+            <SvgBox>
+              <img alt="" src={process.env.PUBLIC_URL + "/img/BG5.svg"} />
+            </SvgBox>
+            <BtnBox style={{ textAlign: "center" }}>
+              <button
+                style={{ marginLeft: "0px" }}
+                onClick={() => {
+                  history.replace(
+                    `/write/portfolio/introduce/${userInfo.porfId}`
+                  );
+                }}
+              >
+                지금 시작하기
+              </button>
+            </BtnBox>
+          </Box>
         </SwiperSlide>
       </Swiper>
     </>
   );
 };
+const Box = styled.div`
+width: 100%;
+height: 100%;
+display: block;
+`;
 
 const BtnBox = styled.div`
   width: 100%;
-  text-align: center;
-  display: flex;
+  text-align: left;
   height: fit-content;
   button {
     margin: 0px auto;
-    padding: 20px 30px;
+    margin-left: 120px;
+    padding: 15px 30px;
     font-size: 18px;
-    background: #00c4b4;
-    border: none;
+    background: inherit;
+    border: 1px solid white;
     border-radius: 43px;
     color: white;
   }
 `;
 const TextBox = styled.div`
-  width: 100%;
+  width: 80%;
   min-width: 600px;
-  height: 500px;
-  border: 1px solid black;
+  height: 350px;
+  padding-top: 100px;
   h1 {
-    margin: 138px 0px 45px 120px;
+    margin: 0px 0px 30px 120px;
     font-family: "Pretendard";
     font-style: normal;
     font-weight: normal;
-    font-size: 56px;
+    font-size: 48px;
     line-height: 65px;
     letter-spacing: -0.01em;
     color: #ffffff;
@@ -185,6 +206,22 @@ const TextBox = styled.div`
     line-height: 30px;
     letter-spacing: -0.01em;
     color: #ffffff;
+  }
+`;
+
+const SvgBox = styled.div`
+  width: fit-content;
+  margin: 0px auto;
+  margin-bottom: 100px;
+  padding-top: 100px;
+`;
+
+const GifBox = styled.div`
+  width: 180px;
+  height: 100px;
+  position: absolute;
+  left: 500px;
+  img{
   }
 `;
 export default Banner;
