af13204c799057345609da75d544e3225458f1dc
han-woo-jin <97419805+han-woo-jin@users.noreply.github.com>
2022-03-30 00:00:25 +0900

    axios 또 다시수정

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -7,8 +7,8 @@ const search = sessionStorage.getItem("search");
 const instance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
 
-  baseURL: "http://3.35.13.186/",
-  // baseURL: "https://jonghun.shop",
+  // baseURL: "http://3.35.13.186/",
+  baseURL: "https://jonghun.shop",
   headers: {
     // "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
     // accept: "*/*",
@@ -20,7 +20,6 @@ const instance = axios.create({
 });
 const testinstance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
-  // baseURL: "http://3.35.13.186/",
   baseURL: "https://jonghun.shop",
   headers: {
     // "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -33,8 +32,8 @@ const testinstance = axios.create({
 });
 const formInstance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
-  baseURL: "http://3.35.13.186/",
-  // baseURL: "https://jonghun.shop",
+  // baseURL: "http://3.35.13.186/",
+  baseURL: "https://jonghun.shop",
   headers: {
     // "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
     // accept: "*/*",
@@ -87,7 +86,6 @@ export const apis = {
   userInfo: () => instance.get("user/info"),
   putStack: (data) => instance.put("user/stack", data),
   putPwd: (data) => instance.put("user/password", data),
-  profileDelete: () => instance.delete("/user/profile"),
 
   introPorf: (data) => instance.put("porf/intro", data),
   introPorfGet: (porfId) => instance.get(`/porf/${porfId}/intro`),
@@ -157,9 +155,4 @@ export const apis = {
 
   gitToken: (token) => instance.put("/user/git/token", token),
   delGitToken: () => instance.delete("/user/git/token"),
-
-  //bookmark
-  addBookmark: (projectId) => instance.post(`/bookmark/project/${projectId}`),
-  deleteBookmark: (projectId) =>
-    instance.delete(`/bookmark/project/${projectId}`),
 };
