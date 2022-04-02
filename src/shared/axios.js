5878424ac9f6126c11b8dea85c27ac7ff5401f51
gwangbaekun <gwangbaekun@gmail.com>
2022-04-02 20:41:23 +0900

    [GIT-ignore]

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -7,8 +7,8 @@ const search = sessionStorage.getItem("search");
 const instance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
 
-  baseURL: "http://3.35.13.186/",
-  // baseURL: "https://jonghun.shop",
+  // baseURL: "http://3.35.13.186/",
+  baseURL: "https://jonghun.shop",
   headers: {
     // "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
     // accept: "*/*",
@@ -20,10 +20,9 @@ const instance = axios.create({
 });
 const formInstance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
-  baseURL: "http://3.35.13.186/",
-  // baseURL: "https://jonghun.shop",
+  // baseURL: "http://3.35.13.186/",
+  baseURL: "https://jonghun.shop",
   headers: {
-
     // "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
     // accept: "*/*",
     "content-type": "multipart/form-data",
