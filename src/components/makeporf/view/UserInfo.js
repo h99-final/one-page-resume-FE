9d5446b8b366b2ff4125131841245e89aaa2bbda
gwangbaekun <gwangbaekun@gmail.com>
2022-04-03 21:36:02 +0900

    [FIX] save alert

--- src/components/makeporf/view/UserInfo.js
+++ src/components/makeporf/view/UserInfo.js
@@ -45,7 +45,7 @@ function UserInfo() {
         dispatch(userActions.userInfoDB());
       })
       .then(() => {
-        // alert("저장되었습니다.");
+        alert("저장되었습니다.");
       })
       .catch((error) => {
         // alert("필수 정보를 입력해주세요");
